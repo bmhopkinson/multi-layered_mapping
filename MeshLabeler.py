1d62d6f48eff7799e772d5d6cc71a986bfc37b7e
Brian Hopkinson <bmhopkin@uga.edu>
2021-09-03 16:25:36 -0400

    added ability to color mesh faces based on images - pred or raw

--- MeshLabeler.py
+++ MeshLabeler.py
@@ -38,6 +38,7 @@ class MeshLabeler():
         self.tree = tree
         self.img_dir = img_dir
         self.n_workers = n_workers
+        self.manager = None
 
         if self.mesh is not None:
             self.vertices = mesh.vertices.view(np.ndarray)
@@ -54,24 +55,29 @@ class MeshLabeler():
 
         return hits_refined
 
-    def extract_triangle_from_prediction_image(self, triangle, image):
+    def extract_triangle_from_image(self, triangle, image):
         # select only the portion of  prediction image within this triangle and covert rgb codes to class data
-        triangle.append(triangle[0]) #close triangle
+
+        #crop image to fit triangle and recompute triangle coordinates
         triangle = np.array(triangle, dtype=int)
-        mask = np.zeros((image.shape[0], image.shape[1]))
+        xy_min = np.amin(triangle, axis=0).astype(int)
+        xy_max = np.amax(triangle, axis=0).astype(int)
+        image_crop = image[xy_min[1]:xy_max[1], xy_min[0]:xy_max[0]]
+        tri_crop = triangle
+        tri_crop[:, 0] = tri_crop[:, 0] - xy_min[0]     #revise x-coords
+        tri_crop[:, 1] = tri_crop[:, 1] - xy_min[1]     #revise y-coords
+        tri_crop = np.append(tri_crop, np.array(tri_crop[0,:], ndmin=2), axis=0) #close triangle
+        mask = np.zeros((image_crop.shape[0], image_crop.shape[1]))
 
         #create mask over triangular region
-        cv2.fillConvexPoly(mask, triangle, 1)
+        cv2.fillConvexPoly(mask, tri_crop, 1)
         mask = mask.astype(np.bool)
-        selection = np.zeros_like(image)
-        selection[mask] = image[mask]  #extract predictions within mask region
+        selection = np.zeros_like(image_crop)
+        selection[mask] = image_crop[mask]  #extract predictions within mask region
+      #  cv2.imshow('selection', selection)
+      #  cv2.waitKey(0)
 
-        # crop for faster processing
-        xy_min = np.amin(triangle, axis=0)
-        xy_max = np.amax(triangle, axis=0)
-        selection_crop = selection[xy_min[1]:xy_max[1], xy_min[0]:xy_max[0]]
-
-        return selection_crop
+        return selection, tri_crop
 
     def maskrgb_to_class(self, mask):
         h, w, channels = mask.shape[0], mask.shape[1], mask.shape[2]
@@ -97,7 +103,7 @@ class MeshLabeler():
 
         return pixel_count / np.sum(pixel_count)
 
-    def process_frames(self, frames, cover):
+    def process_frames_prediction(self, frames, cover):
         for frame in frames:
             print('working on {}, id: {}'.format(frame.label, frame.frame_id))
             hits, aabbs = frame.project_from_tree(self.tree, descend=4)
@@ -109,29 +115,37 @@ class MeshLabeler():
 
             for face in hits_refined:
                 triangle = hits_refined[face]
-                selection = self.extract_triangle_from_prediction_image(triangle, img_pred)
+                selection, tri_coords = self.extract_triangle_from_image(triangle, img_pred)
                 class_data = self.maskrgb_to_class(selection)
                 fc = self.fractional_cover_from_selection(class_data)
 
                 key = str(face) + "_" + str(frame.frame_id)
                 cover[key] = np.array(fc, ndmin=2)
 
-                # if face in cover:
-                #     cover[face] = np.append(cover[face], np.array(fc,ndmin=2), axis=0)
-                # else:
-                #     cover[face] = np.array(fc, ndmin=2)
-
         return cover
+
+    def collate_results(self, raw_ds):   #takes raw dictionaries with multiple observations per face (different frames) and collates them by face
+        collated_ds = {}
+        for obs in raw_ds:
+            m = parse_cover_key.search(obs)
+            face = int(m.group(1))
+            if face in collated_ds:
+                collated_ds[face] = np.append(collated_ds[face], np.array(raw_ds[obs],ndmin=2), axis=0)
+            else:
+                collated_ds[face] = np.array(raw_ds[obs], ndmin=2)
+
+        return collated_ds
 
     def from_frame_interval(self, start=0,stop=0):
         frames_selection = self.frames[start:stop]
 
-        manager = mp.Manager()
-        cover_raw = manager.dict()  #this will hold fractional cover data a: key 'faceid_frame_id', value: list of fractional cover,
+        if self.manager is None:
+            self.manager = mp.Manager()
+        cover_raw = self.manager.dict()  #this will hold fractional cover data a: key 'faceid_frame_id', value: list of fractional cover,
                                     # this structure is simpler to deal with in multiprocesing context and will be postproccesed later
         jobs = []
         for chunk in chunks(frames_selection, math.ceil(len(frames_selection) / self.n_workers)):
-            j = mp.Process(target=self.process_frames,
+            j = mp.Process(target=self.process_frames_prediction,
                            args=(chunk, cover_raw))
             j.start()
             jobs.append(j)
@@ -139,14 +153,7 @@ class MeshLabeler():
         for j in jobs:
             j.join()
 
-        cover = {}  # face_id: n_obs x n_class nparray holding fracional cover observation for obs_i, class_i
-        for obs in cover_raw:
-            m = parse_cover_key.search(obs)
-            face = int(m.group(1))
-            if face in cover:
-                cover[face] = np.append(cover[face], np.array(cover_raw[obs],ndmin=2), axis=0)
-            else:
-                cover[face] = np.array(cover_raw[obs], ndmin=2)
+        cover = self.collate_results(cover_raw)
 
         cover_avg = {}  #face_id: averaged_fractional_cover (over all observations of face)
         for face in cover:
@@ -173,6 +180,75 @@ class MeshLabeler():
             fout.write('\n')
 
         fout.close()
+
+    def _color_faces_from_images(self, frames, face_colors,  image_folder, ext):
+        for frame in frames:
+            print('working on {}, id: {}'.format(frame.label, frame.frame_id))
+            hits, aabbs = frame.project_from_tree(self.tree, descend=4)
+            hits_refined = self.refine_hits(frame, hits)
+
+            img_path = image_folder + frame.label + ext
+            img = cv2.imread(img_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            for face in hits_refined:
+                triangle = hits_refined[face]
+                selection, tri_coords = self.extract_triangle_from_image(triangle, img)
+                mask = np.zeros((selection.shape[0], selection.shape[1]), dtype=np.uint8)
+                cv2.fillConvexPoly(mask, tri_coords, 255)
+                color = cv2.mean(selection, mask)
+                key = str(face) + "_" + str(frame.frame_id)
+                #face_colors[key] = {'color': color[0:3], 'coords': tri_coords}
+                face_colors[key] = color[0:3]
+
+        return face_colors
+
+
+    def color_faces_from_images_interval(self, start, stop, image_folder, ext):
+        frames_selection = self.frames[start:stop]
+
+        if self.manager is None:
+            self.manager = mp.Manager()
+
+        face_colors_raw = self.manager.dict()  # this will hold face color data a: key 'faceid_frame_id', value: list of face colors,
+        # this structure is simpler to deal with in multiprocesing context and will be postproccesed later
+        jobs = []
+        for chunk in chunks(frames_selection, math.ceil(len(frames_selection) / self.n_workers)):
+            j = mp.Process(target=self._color_faces_from_images,
+                           args=(chunk, face_colors_raw, image_folder, ext))
+            j.start()
+            jobs.append(j)
+
+        for j in jobs:
+            j.join()
+
+        face_colors = self.collate_results(face_colors_raw)
+
+        face_colors_avg = {}  # face_id: averaged_fractional_cover (over all observations of face)
+        for face in face_colors:
+            face_colors_avg[face] = np.mean(face_colors[face], axis=0)
+
+        for face in face_colors_avg:
+            colors = face_colors_avg[face]
+            self.mesh.visual.face_colors[face] = np.array([colors[0], colors[1], colors[2], 255], dtype=np.uint8)
+
+        fout = open('face_color_test.txt', 'w')
+        for face in face_colors:
+            fout.write('{:d}'.format(face))
+            for c in face_colors_avg[face]:
+                fout.write('\t{:d}'.format(int(c)))
+            fout.write('\n')
+
+        fout.close()
+
+        return self.mesh
+
+    def color_faces_from_images_all(self, image_folder, ext):
+        start = 0
+        stop = len(self.frames)
+        return self.color_faces_from_images_interval(start, stop, image_folder, ext)
+
+
 
 
 
--- main.py
+++ main.py
@@ -7,24 +7,11 @@ from MeshLabeler import MeshLabeler
 from aabbtree_mod import AABB, AABBTree
 import json
 
-mesh_file = './data/Sapelo_202106_run15/mesh.ply'
+mesh_file = './data/Sapelo_202106_run15/mesh_fine.ply'
 camera_file = './data/Sapelo_202106_run15/agisoft_cameras_Imaging.xml'
 image_folder = './data/Sapelo_202106_run15/imaging_preds_3by2/'
+image_raw_folder ='./data/Sapelo_202106_run15/imaging/'
 
-# n_classes =9
-# class_map = {  # RGB to Class
-#     (0, 0, 0): -1,  # out of bounds
-#     (255, 255, 255): 0,  # background
-#     (150, 255, 14): 0,  # Background_alt
-#     (127, 255, 140): 1,  # Spartina
-#     (113, 255, 221): 2,  # dead Spartina
-#     (99, 187, 255): 3,  # Sarcocornia
-#     (101, 85, 255): 4,  # Batis
-#     (212, 70, 255): 5,  # Juncus
-#     (255, 56, 169): 6,  # Borrichia
-#     (255, 63, 42): 7,  # Limonium
-#     (255, 202, 28): 8  # Other
-# }
 
 def load_mesh():
     mesh = trimesh.load_mesh(mesh_file)
@@ -56,31 +43,6 @@ def load_agisoft_data():
 
     return cameras, frames
 
-# def maskrgb_to_class(mask):
-#     h, w, channels = mask.shape[0], mask.shape[1], mask.shape[2]
-#     mask_out = -1*np.ones((h, w), dtype=int)
-#
-#     for k in class_map:
-#         matches = np.zeros((h, w ,channels), dtype=bool)
-#
-#         for c in range(channels):
-#            matches[:,:,c] = mask[:,:,c] == k[c]
-#
-#         matches_total = np.sum(matches, axis=2)
-#         valid_idx = matches_total == channels
-#         mask_out[valid_idx] = class_map[k]
-#
-#     return mask_out
-#
-# def fractional_cover_from_selection(class_data):
-#     pixel_count = []
-#     for i in range(n_classes):
-#         t = np.sum(class_data == i)
-#         pixel_count.append(t)
-#
-#     return pixel_count/np.sum(pixel_count)
-
-
 
 if __name__ == '__main__':
 
@@ -94,9 +56,11 @@ if __name__ == '__main__':
     cameras, frames = load_agisoft_data()
 
     labeler = MeshLabeler(frames=frames, mesh=mesh, tree=tree,img_dir=image_folder, n_workers=20)
- #   labels, mesh = labeler.from_frame_interval(0, 60)
-    labels, mesh = labeler.from_all_frames()
-    labeler.write_labels(labels, 'test.txt')
+    mesh = labeler.color_faces_from_images_all(image_raw_folder, '.jpg')
+    #mesh = labeler.color_faces_from_images(image_folder, '_pred.png')
+    #labels, mesh = labeler.from_frame_interval(0, 120)
+    #labels, mesh = labeler.from_all_frames()
+    #labeler.write_labels(labels, 'test.txt')
 
     mesh.show()
 
